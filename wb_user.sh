d9caa4c37731cc9d925c0d04a3d57585cfb483c1
Wallace Bruno Gentil <wallace.gentil@superare.co>
2019-12-30 10:17:57 -0300

    add - Check if exists  and  || Check if  is .CSV file and  is valid option

---
+++ wb_user.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#+----------+
+#+-- VARS --+
+#+----------+
+
+declare -a C_FULLNAME
+declare -a C_UNAME
+declare -a C_PASSWORD
+declare -a C_GROUP
+SAIDA=''
+
+#+--------------+
+#+ -- checks -- +
+#+--------------+
+
+if [ -z "$1" ]; then
+    printf -e 'Args 1: Invalid please use:
+    bash wb_user.sh /home/{USER}/path/list_user.csv [$2]
+    '
+    exit 1
+fi
+
+if [ ! -f $1 ] && [ ! $1 == *.csv ]; then
+    printf 'Please use a .CSV file \n'
+    exit 1
+fi
+
+if [ -z "$2" ]; then
+cat <<EOF
+    Args 2: Invalid please use:
+    -new           : To create user;
+    -rm            : To remove user;
+    -lock         : To block accont user;
+    -unlock        : To unlock accont user;
+    -update        : To update a value to user:
+EOF
+fi
