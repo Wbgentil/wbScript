b5edccea7ff33a804c8d8a5bfd41c6385357eff0
Wallace Bruno Gentil <wallace.gentil@superare.co>
2019-12-30 11:27:25 -0300

    add - CSV_READ() function to teste a CSV read || CSV format

--- wb_user.sh
+++ wb_user.sh
@@ -9,9 +9,28 @@ declare -a C_PASSWORD
 declare -a C_GROUP
 SAIDA=''
 
-#+--------------+
-#+ -- checks -- +
-#+--------------+
+#+----------+
+#+-- FUNC --+
+#+----------+
+
+CSV_READ(){
+   while IFS=, read -r fullname uname password groups others; do
+      C_UNAME+=($uname)
+      C_PASSWORD+=($password)
+      C_GROUP+=($groups)
+   done <"$1"
+
+   echo "${!C_UNAME[@]}"
+
+   for i in "${!C_UNAME[@]}"; do
+      printf "Username: ${C_UNAME[$i]}\n"
+      printf "Password: ${C_PASSWORD[$i]}\n"
+      printf "Grupo: ${C_GROUP[$i]}\n\n"
+   done
+}
+#+----------+
+#+-- CHEK --+
+#+----------+
 
 if [ -z "$1" ]; then
    printf -e 'Args 1: Invalid please use:
@@ -20,7 +39,17 @@ if [ -z "$1" ]; then
    exit 1
 fi
 
-if [ ! -f $1 ] && [ ! $1 == *.csv ]; then
+if [ "$1" == "man" ] || [ "$1" == "MAN" ]; then
+   printf "To use this script use a follow example:
+   wb_user.sh /home/user/docs/file.csv -opt
+   
+   Where: \$1 is .CSv file path
+          \$2 is option\n"
+
+   exit 1
+fi
+
+if [ ! -f $1 ] && [ ! $1 == *.csv ] ; then
    printf 'Please use a .CSV file \n'
    exit 1
 fi
@@ -38,7 +67,7 @@ fi
 
 case $2 in 
    -new)
-      echo "new"
+      CSV_READ $1
    ;; -rm)
       echo "rm"
    ;; -update)
